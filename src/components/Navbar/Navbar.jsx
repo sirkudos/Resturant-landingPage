c01352fc7394262888d4a277ea6624dfabe449f7
ikechukwu <attahikechukwu@gmail>
2022-02-05 00:17:11 +0100

    created navbar component

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { MdOutlineRestaurantMenu } from 'react-icons/gi';
+import images from '../../constants/images';
+import './Navbar.css';
+
+const Navbar = () => (
+  <div>
+    Navbar
+  </div>
+);
+
+export default Navbar;
